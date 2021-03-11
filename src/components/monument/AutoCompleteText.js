495a0b6899e10e8a8953f67e984e3e990103734b
amarcottec <72509009+amarcottec@users.noreply.github.com>
2021-03-10 21:06:48 -0500

    create logic for search autotext bar

--- src/components/monument/AutoCompleteText.js
+++ src/components/monument/AutoCompleteText.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import './AutoCompleteText.css' 
-
 export default class AutoCompleteText extends Component {
     
     constructor(props){
         super(props);
+
         this.state = {
             suggestions: [],
             text: '',
@@ -19,8 +19,8 @@ export default class AutoCompleteText extends Component {
     }
 
     onTextChanged = async (e) => {
-        const { items } = this.props;
         const value = e.target.value;
+        const { items } = this.props;
         let suggestions = [];
         if (value.length > 0){ 
             suggestions = items.filter(v => v.toLowerCase().includes(value.toLowerCase()) );
@@ -29,6 +29,12 @@ export default class AutoCompleteText extends Component {
             suggestions: suggestions,
             text: value,
         }));
+    }
+
+    onChercher = () => {
+        const text = this.state.text;
+        const { updateInput } = this.props;
+        updateInput(text);
     }
 
     renderSuggestions() {
@@ -40,18 +46,28 @@ export default class AutoCompleteText extends Component {
         
         return (
             <ul>
-            {suggestions.map((item, key) => <li id={key}  onClick={ () => this.suggestionSelected(item)}>{item}</li>)}
+            {suggestions.map((item, index) => <li key={index}  onClick={ () => this.suggestionSelected(item)}>{item}</li>)}
             </ul>
         );
     }
     
     render() {
         const {text} = this.state;
+       
         return (
-            <div className='AutocompleteText'>
-                <input value={text} type='text' onChange={this.onTextChanged}/>
-                {this.renderSuggestions()}
+            <>
+            <div className='AutoCompleteSearch'>
+                <div className='AutocompleteText'>
+                    <input value={text} type='text' onChange={this.onTextChanged}/>
+                    {this.renderSuggestions()}
+                    
+                </div>
+                <div>
+                <button className='SearchButton' onClick={this.onChercher}>Chercher</button>
+                </div>
             </div>
+            </>
+             
         )
     }
 }
--- src/components/monument/Monument.js
+++ src/components/monument/Monument.js
@@ -54,6 +54,7 @@ export default class Monument extends Component {
   clicked(area) {
     let imgNumber  = Math.floor(Math.random() * 370)
     this.props.history.push("/monument/" + imgNumber.toString());
+
   }
 
   enterArea(area) {
@@ -73,15 +74,22 @@ export default class Monument extends Component {
 
     return { top: `${area.center[1]}px`, left: `${area.center[0]}px` };
   }
+
+ 
+  updateInput  =  (name) => {
+    const monument = monumentData.find(monu => monu.name === name);
+    this.props.history.push("/monument/" + monument.slug);
+  }
     
   render() {
     
   const width = this.state.width;
   
+
     return (
       <>
       <div className='AutoCompleteText-Component'>
-      <AutoCompleteText items={this.state.persons}/>
+      <AutoCompleteText items={this.state.persons} updateInput={this.updateInput}/>
       </div>
       
         <section className="filter-container">
